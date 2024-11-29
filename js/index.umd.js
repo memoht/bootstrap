08c84ddb4873cd61a5802da85e3aa6b6e7527355
guillermo haas-thompson <guillermo@haasthompson.com>
2024-11-28 21:57:20 -0500

    Revert "try dropping carousel, popover and a couple of others"
    This reverts commit 1e55326e7e83dd8ced12d401abe861fe3ec04ff0.

--- js/index.umd.js
+++ js/index.umd.js
@@ -7,12 +7,12 @@
 
 import Alert from './src/alert.js'
 import Button from './src/button.js'
-// import Carousel from './src/carousel.js'
+import Carousel from './src/carousel.js'
 import Collapse from './src/collapse.js'
 import Dropdown from './src/dropdown.js'
 import Modal from './src/modal.js'
 import Offcanvas from './src/offcanvas.js'
-// import Popover from './src/popover.js'
+import Popover from './src/popover.js'
 import ScrollSpy from './src/scrollspy.js'
 import Tab from './src/tab.js'
 import Toast from './src/toast.js'
@@ -21,12 +21,12 @@ import Tooltip from './src/tooltip.js'
 export default {
   Alert,
   Button,
-  // Carousel,
+  Carousel,
   Collapse,
   Dropdown,
   Modal,
   Offcanvas,
-  // Popover,
+  Popover,
   ScrollSpy,
   Tab,
   Toast,
